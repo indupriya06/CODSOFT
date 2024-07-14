ac7c3a88d06b73e5fbecdd2e33eacf0452c2b367
indupriya06 <166099784+indupriya06@users.noreply.github.com>
2024-07-14 18:58:58 +0530

    Add files via upload

---
+++ password_Generator.py
@@ -0,0 +1,42 @@
+import re
+import secrets
+import string
+
+user_input = int(input("Enter the desired length of the password:"))
+
+def generate_password(length=user_input, nums=1, special_chars=1, uppercase=1, lowercase=1): 
+    "This function accepts a parameter 'len' and returns a randomly generated password"  
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        # Check constraints        
+        if all(
+            constraint <= len(re.findall(pattern, password))
+            for constraint, pattern in constraints
+        ):
+            break
+    
+    return password
+if __name__ == '__main__':    
+    new_password = generate_password()
+    print(new_password)
+
+
